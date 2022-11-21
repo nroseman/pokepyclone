81e0324a9f84471e87c5973b2e42e51396ad1f02
Nroseman <nicholas.roseman@gmail.com>
2022-11-21 09:19:56 -0500

    Pokeclone minor update

--- enemy.py
+++ enemy.py
@@ -4,6 +4,6 @@ import pygame
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, pos, path, layer, groups):
         super().__init__(groups)
-        self.image = pygame.Surface((200, 100))
+        self.image = pygame.Surface((64, 64))
         self.rect = self.image.get_rect(topleft=pos)
         self.z = layer
